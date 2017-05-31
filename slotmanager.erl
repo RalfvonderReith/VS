b92c85f75da14a34457dec660f95076d4aed36f9
Ralf <ralf.vonderreith@haw-hamburg.de>
2017-05-31 11:45:37 +0200

    added slotM STationNumber

--- slotmanager.erl
+++ slotmanager.erl
@@ -1,37 +1,35 @@
 -module(slotmanager).
 -export([start/1]).
 
--define(logfile, "slots.log").
-
-start(Size) ->
+start(Size, SNo) ->
 	SlotList = lists:seq(1, Size),
-	communicate(SlotList, Size).
+	communicate(SlotList, Size, SNo).
 	
-communicate(SlotList, Size) ->
+communicate(SlotList, Size, SNo) ->
 	io:format("SlotList ~p~n", [SlotList]),
 	receive
 		{new_frame} ->
-			werkzeug:logging(?logfile, "Station: A new frame has been started\r\n"),
-			communicate(lists:seq(1, Size), Size);
+			werkzeug:logging(lists:concat(["slotM", SNo, ".log"]), "Station: A new frame has been started\r\n"),
+			communicate(lists:seq(1, Size), Size, SNo);
 		{reserve, Slot} ->
-			werkzeug:logging(?logfile, lists:concat(["Slot ", Slot, "Station:  has been reserved.\r\n"])),
-			communicate(reserveSlot(SlotList, Slot), Size);
+			werkzeug:logging(lists:concat(["slotM", SNo, ".log"]), lists:concat(["Slot ", Slot, "Station:  has been reserved.\r\n"])),
+			communicate(reserveSlot(SlotList, Slot), Size, SNo);
 		{get_slot, PID} -> 
 			Slot = getSlot(SlotList),
 			PID ! {slot, Slot},
-			werkzeug:logging(?logfile, lists:concat(["Slot ",Slot, "Station:  has been chosen for next frame.\r\n"])),
-			communicate(reserveSlot(SlotList, Slot), Size);
+			werkzeug:logging(lists:concat(["slotM", SNo, ".log"]), lists:concat(["Slot ",Slot, "Station:  has been chosen for next frame.\r\n"])),
+			communicate(reserveSlot(SlotList, Slot), Size, SNo);
 		Any ->
 			io:format("Station: unexpected message: ~p~n",[Any])
 	end.
 
 %reserves a specified slot
-reserveSlot(SlotList, Slot) -> reserveSlotH(SlotList, [], Slot).
-reserveSlotH([], CheckedSlots, Slot) -> 
-	werkzeug:logging(?logfile, lists:concat(["Station: Slot ", Slot, " already reserved \r\n"])),
+reserveSlot(SlotList, Slot, SNo) -> reserveSlotH(SlotList, [], Slot, SNo).
+reserveSlotH([], CheckedSlots, Slot, SNo) -> 
+	werkzeug:logging(lists:concat(["slotM", SNo, ".log"]), lists:concat(["Station: Slot ", Slot, " already reserved \r\n"])),
 	CheckedSlots;
-reserveSlotH([Slot|Rest], CheckedSlots, Slot) -> lists:concat([CheckedSlots, Rest]);
-reserveSlotH([First|Rest], CheckedSlots, Slot) -> reserveSlotH(Rest, lists:append(CheckedSlots, [First]), Slot).
+reserveSlotH([Slot|Rest], CheckedSlots, Slot, SNo) -> lists:concat([CheckedSlots, Rest]);
+reserveSlotH([First|Rest], CheckedSlots, Slot, SNo) -> reserveSlotH(Rest, lists:append(CheckedSlots, [First]), Slot).
 
 %retrieves a random entry from the list
 getSlot([]) -> -1;
